802026e951e390eadbc9a432b286f6b678a611e3
Xavier <felipe.xavier@solve4me.com.br>
2025-01-31 14:09:30 -0300

    Changed the entrypoint script

--- webchat/mosquitto/scripts/entrypoint.sh
+++ webchat/mosquitto/scripts/entrypoint.sh
@@ -5,8 +5,11 @@ if [ ! -f "$PASSWORD_FILE" ]; then
   PASSWORD1=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
   PASSWORD2=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
 
-  echo "router4me_server:$PASSWORD1" > "$PASSWORD_FILE"
-  echo "router4me_client:$PASSWORD2" >> "$PASSWORD_FILE"
+  mosquitto_passwd -c -b "$PASSWORD_FILE" "router4me_server" "$PASSWORD1"
+  mosquitto_passwd -b "$PASSWORD_FILE" "router4me_client" "$PASSWORD2"
+
+  echo "router4me_server:$PASSWORD1"
+  echo "router4me_client:$PASSWORD2"
 fi
 
 /docker-entrypoint.sh mosquitto
