0ad67433c6378bf4feaefb975de3d03b964b12b5
Xavier <felipe.xavier@solve4me.com.br>
2025-01-31 14:57:22 -0300

    Changed the webchat solution

--- webchat/mosquitto/scripts/entrypoint.sh
+++ webchat/mosquitto/scripts/entrypoint.sh
@@ -1,5 +1,16 @@
-#!/bin/sh 
-PASSWORD_FILE="/mosquitto/config/passwordfile"
+#!/bin/sh
+ACLS_FILE="/mosquitto/config/mosquitto_acls"
+PASSWORD_FILE="/mosquitto/config/passwd"
+
+if [ ! -f "$ACLS_FILE" ]; then
+  echo "user router4me_server" > $ACLS_FILE
+  echo "topic readwrite" >> $ACLS_FILE
+
+  echo "user router4me_client" >> $ACLS_FILE
+  echo "topic read" >> $ACLS_FILE
+
+  chmod 0700 $ACLS_FILE
+fi
 
 if [ ! -f "$PASSWORD_FILE" ]; then
   PASSWORD1=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
@@ -8,8 +19,10 @@ if [ ! -f "$PASSWORD_FILE" ]; then
   mosquitto_passwd -c -b "$PASSWORD_FILE" "router4me_server" "$PASSWORD1"
   mosquitto_passwd -b "$PASSWORD_FILE" "router4me_client" "$PASSWORD2"
 
+  echo "*****PASSWORDS*******"
   echo "router4me_server:$PASSWORD1"
   echo "router4me_client:$PASSWORD2"
+  echo "*****PASSWORDS*******"
 fi
 
-/docker-entrypoint.sh mosquitto
+/docker-entrypoint.sh mosquitto -c "/mosquitto/config/mosquitto.conf"
