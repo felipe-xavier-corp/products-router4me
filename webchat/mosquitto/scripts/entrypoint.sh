2592255c060f11fafb274b691466d78f87773653
Xavier <felipe.xavier@solve4me.com.br>
2025-01-31 13:49:44 -0300

    Changed the acsl

--- webchat/mosquitto/scripts/entrypoint.sh
+++ webchat/mosquitto/scripts/entrypoint.sh
@@ -2,10 +2,11 @@
 PASSWORD_FILE="/mosquitto/config/passwordfile"
 
 if [ ! -f "$PASSWORD_FILE" ]; then
-  USER="router4me"
-  PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
+  PASSWORD1=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
+  PASSWORD2=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
 
-  echo "$USER:$PASSWORD" > "$PASSWORD_FILE"
+  echo "router4me_server:$PASSWORD1" > "$PASSWORD_FILE"
+  echo "router4me_client:$PASSWORD2" > "$PASSWORD_FILE"
 fi
 
 /docker-entrypoint.sh mosquitto
